a9d0de2ca3ca781cdcb0c2c531f6b536303ec8cc
Ismael José Jiménez <145917683+Wixma17@users.noreply.github.com>
2024-06-24 15:08:25 +0200

    Backup proyecto front y back
    1 commit de front y end  creado los proyectos base

---
+++ rusticasaback/src/main/java/com/rusticasaback/rusticasaback/RusticasabackApplication.java
@@ -0,0 +1,13 @@
+package com.rusticasaback.rusticasaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RusticasabackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RusticasabackApplication.class, args);
+	}
+
+}
---
+++ rusticasaback/src/test/java/com/rusticasaback/rusticasaback/RusticasabackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rusticasaback.rusticasaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RusticasabackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
