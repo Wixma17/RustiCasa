9ea4c3bf4b4247aea3d50c585e3088c8ff3a020d
Ismael José Jiménez <145917683+Wixma17@users.noreply.github.com>
2024-07-26 18:39:51 +0200

    implementacion barra busqueda
    queda poner la logica de las busquedas

--- rusticasaFront/src/app/containers/header/header.module.ts
+++ rusticasaFront/src/app/containers/header/header.module.ts
@@ -1,13 +1,18 @@
+import { InputTextModule } from 'primeng/inputtext';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+
 
 
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    InputTextModule,
+    FormsModule
   ],
   exports: [
     HeaderComponent
--- rusticasaFront/src/app/containers/header/header/header.component.ts
+++ rusticasaFront/src/app/containers/header/header/header.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  nombreCasa:string ='';
+  resultados: string[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  buscar(): void {
+    console.log('Buscando:', this.nombreCasa);
   }
 
 }
