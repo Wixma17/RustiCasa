c61d1fb7c1ac54f7b8fa3d256e7714923e48b354
Ismael José Jiménez <145917683+Wixma17@users.noreply.github.com>
2024-08-03 01:07:31 +0200

    Update OpinaRepository.java
    quitado los cambios de obtener opinion por idCasa

--- rusticasaback/src/main/java/com/rusticasaback/rusticasaback/repositories/OpinaRepository.java
+++ rusticasaback/src/main/java/com/rusticasaback/rusticasaback/repositories/OpinaRepository.java
@@ -1,13 +1,10 @@
 package com.rusticasaback.rusticasaback.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
 import com.rusticasaback.rusticasaback.entities.relaciones.OpinaEntity;
 import com.rusticasaback.rusticasaback.entities.relaciones.OpinaEntityPK;
 
 
 public interface OpinaRepository extends JpaRepository<OpinaEntity, OpinaEntityPK>{
-    @Query("Select o From OpinaEntityPK o where o.idCasa like :idCasa")
-    OpinaEntity findByIdCasa(Long idCasa);
+   
 }
